2e81a7c3814ffbc8e6b72250d5bffce36dee16cb
sherif shalaby <54622122+sherifMshalaby@users.noreply.github.com>
2020-09-17 12:30:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,17 +12,5 @@ module.exports = {
         iFrameSecret: "c1b1515e-dfb3-4c10-8362-23e5b8df4cff",
         HPPSecret:"46a42c88-ff18-4810-a60b-02ad142ac23a",        
         mid: "MID-437-890"
-    },
-    "staging-live": {
-        baseUrl: "https://iframe.payformance.io",
-        iFrameSecret: "",
-        HPPSecret:"",        
-        mid: ""
-    },
-    "staging-test": {
-        baseUrl: "https://test-iframe.payformance.io",
-        iFrameSecret: "285126c8-cb13-4ef5-bb02-491144145b97",
-        HPPSecret:"cdfd5c4c-4ddd-4fb7-9460-1f393a4bd2b6",
-        mid: "MID-24-989"
     }
 }

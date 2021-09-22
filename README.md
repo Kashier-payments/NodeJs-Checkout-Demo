de47509dc9e23ed024c35abc61ef85511a248e72
Mohamed Khaled <mshaaban@kashier.io>
2021-09-22 13:05:33 +0200

    upgrade

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # NodeJs-Checkout-Demo
 Create and pay orders through IFrame and Hosted Payment Page in Nodejs.
-
-At root folder open terminal and type
-
-    node app.js
-
-Navigate to localhost:9000
--- backend.js
+++ backend.js
@@ -13,24 +13,33 @@ module.exports = {
         const hash = crypto.createHmac('sha256', secret)
             .update(path)
             .digest('hex');
-            console.log(hash);
-        return hash;
+             return hash;
     },
     validateSignature: function (query,secret) {
-        let queryString = "";
-        for (let key in query) {
-            if (key == "signature" || key == "mode")
-                continue;
-            queryString = queryString + "&" + key + "=" + query[key];
-        }
+   let queryString = 
+   "&paymentStatus="+query["paymentStatus"]+
+   "&cardDataToken="+query["cardDataToken"]+
+   "&maskedCard="+query["maskedCard"]+
+   "&merchantOrderId="+query["merchantOrderId"]+
+   "&orderId="+query["orderId"]+
+   "&cardBrand="+query["cardBrand"]+
+   "&orderReference="+query["orderReference"]+
+   "&transactionId="+query["transactionId"]+
+   "&amount="+query["amount"]+
+   "&currency="+query["currency"];
+   
         let finalUrl = queryString.substr(1);
         const signature = crypto.createHmac('sha256', secret)
             .update(finalUrl)
             .digest('hex');
       
-        if (signature == query.signature)
+        if (signature == query.signature){
+            console.log("Success Signature")
             return true;
-        else
+        }else{
+            console.log("Failed Signature !!")
             return false;
+        }
+           
     }
 }
